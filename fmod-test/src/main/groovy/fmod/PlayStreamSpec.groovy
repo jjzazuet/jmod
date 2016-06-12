2c55bbaf6d8ee839d40e118943f9196e4c37a65d
Jesus Zazueta <jjzazuet@gmail.com>
2016-06-12 01:59:09 -0400

    - Integration test case fixes.

--- fmod-test/src/main/groovy/fmod/PlayStreamSpec.groovy
+++ fmod-test/src/main/groovy/fmod/PlayStreamSpec.groovy
@@ -1,16 +1,25 @@
 package fmod
 
+import net.tribe7.fmod.FMOD_RESULT
+import net.tribe7.fmod.System
+import net.tribe7.fmod.fmod
 import spock.lang.Specification
+
+import java.nio.LongBuffer
 
 class PlayStreamSpec extends Specification {
 
-   void setupSpec() {
+    void setupSpec() {
+        java.lang.System.load(new File('../fmod/linux/lib/amd64/libfmod.so').absolutePath)
+        java.lang.System.load(new File('../fmod/build/libs/fmod-jni/shared/libfmod-jni.so').absolutePath)
+    }
 
-   }
-
-   void "Play Stream."() {
-      File f = new File('.')
-      println  f.absolutePath
-      expect: true
-   }
+    void "Play Stream."() {
+        System s = fmod.System_Create()
+        LongBuffer lb = LongBuffer.allocate(1)
+        FMOD_RESULT res = s.getVersion(lb)
+        s.close()
+        s.release()
+        expect: true
+    }
 }
