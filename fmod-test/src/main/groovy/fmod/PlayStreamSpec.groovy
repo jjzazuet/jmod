cca8eec1845d6b7845f6d7f8a1b6d8440c00104d
Jesus Zazueta <jjzazuet@gmail.com>
2016-06-11 23:46:56 -0400

    - Changing internal jar bundle lib name (maybe it will be easier to load at runtime).

--- fmod-test/src/main/groovy/fmod/PlayStreamSpec.groovy
+++ fmod-test/src/main/groovy/fmod/PlayStreamSpec.groovy
@@ -4,4 +4,13 @@ import spock.lang.Specification
 
 class PlayStreamSpec extends Specification {
 
+   void setupSpec() {
+
+   }
+
+   void "Play Stream."() {
+      File f = new File('.')
+      println  f.absolutePath
+      expect: true
+   }
 }
